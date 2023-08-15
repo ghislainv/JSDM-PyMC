2ed729330be1918d7a13c312dd3fb3eed188d81d
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2023-08-15 18:47:13 +0200

    Making a function for simulating data

--- model-with-sim-data.R
+++ model-with-sim-data.R
@@ -8,69 +8,25 @@
 ## Libraries
 library(jSDM)
 
-## Output directory
+## Simulating data
+source("sim-data.R")
 out_dir <- "outputs/R_sim_data/"
-dir.create(out_dir)
-
-## ==================
-## Data simulation
-## ==================
-
-## Set seed
-seed <- 1234
-set.seed(seed)
-
-## Variables
-n_species <- 30  ## Number of species
-n_sites <- 100  ## Number of sites
-n_p <- 3  ## Number of explanatory variables (including intercept)
-n_q <- 3  ## Number of latent variables
-
-## Explanatory variables X
-x <- matrix(rnorm(n_sites * (n_p - 1), 0, 1), nrow=n_sites, ncol=(n_p - 1))
-X <- cbind(rep(1, n_sites), x)
-colnames(X) <- c("Intercept", "x1", "x2")
-
-## Latent factors W
-W <- matrix(rnorm(n_sites * n_q, 0, 1), nrow=n_sites, ncol=n_q)
-
-## Fixed species effect beta 
-beta_target <- matrix(runif(n_species * n_p, -1, 1), nrow=n_p, ncol=n_species)
-
-## Factor loadings lambda  
-mat0 <- matrix(runif(n_species * n_q, -1, 1), nrow=n_q, ncol=n_species)
-axis_imp <- seq(n_q, 1, -1)  ## Decreasing importance of the latent axis
-mat <- mat0 * axis_imp
-lambda_target <- matrix(0, n_q, n_species)
-lambda_target[upper.tri(mat, diag=TRUE)] <- mat[upper.tri(mat, diag=TRUE)]
-# Small negative values on the diagonal
-diag(lambda_target) <- rep(-0.1, n_q)
-# Large positive values for some species
-for (i in 1:n_q) {
-  lambda_target[i, n_q + i] <- n_q - i + 1
-  if (i + 1 <= n_q) {
-    lambda_target[(i + 1):n_q, n_q + i] <- 0
-  }
-}
-
-## Variance of random site effects
-V_alpha_target <- 0.1
-## Random site effect alpha
-alpha_target <- rnorm(n_sites, mean=0, sd=sqrt(V_alpha_target))
-
-## Simulation of response data with probit link
-## Latent variable Z
-e <- matrix(rnorm(n_species * n_sites, 0, 1), n_sites, n_species)
-Z <- alpha_target + X %*% beta_target + W %*% lambda_target + e
-# Presence-absence matrix Y
-Y <- matrix(0, n_sites, n_species)
-Y[Z > 0] <- 1
-
-## Save data
-Y <- data.frame(Y)
-colnames(Y) <- paste("sp", 1:n_species, sep="_")
-rownames(Y) <- paste("site", 1:n_sites, sep="_")
-write.csv(Y, file=file.path(out_dir, "Y.csv"), row.names=FALSE)
+data <- sim_data_LVJSDM(n_species=30,
+                        n_sites=100,
+                        n_p=3,  # Number of explanatory variables (including intercept)
+                        n_q=3,  # Number of latent variables
+                        V_alpha_target=0.1,  # Variance of random site effects
+                        neg_val_diag=-0.1, # Small negative values on the diagonal
+                        seed=1234,
+                        out_dir=out_dir)
+X <- data$X
+Y <- data$Y
+W <- data$W
+beta_target <- data$beta_target
+lambda_target <- data$lambda_target
+n_sites <- nrow(X)
+n_q <- ncol(X)
+n_species <- ncol(Y)
 
 ## =======================================
 ## Model 1
@@ -119,7 +75,7 @@ for (j in 1:n_species) {
 ## Species effects beta
 pdf(file=file.path(out_dir, "pred_obs_beta_mod_1.pdf"), width=5, height=5)
 plot(t(beta_target), mean_beta,
-     main="species effect beta",
+     main="Species effect beta",
      xlab ="obs", ylab ="fitted")
 abline(a=0,b=1,col='red')
 dev.off()
@@ -129,7 +85,7 @@ pdf(file=file.path(out_dir, "pred_obs_lambda_mod_1.pdf"), width=10, height=10)
 par(mfrow=c(2, 2))
 for (l in 1:n_q) {
   plot(t(lambda_target)[, l], mean_lambda[, l],
-       main=paste0("factor loadings lambda", l),
+       main=paste0("Factor loadings lambda", l),
        xlab ="obs", ylab ="fitted")
   abline(a=0,b=1,col='red')
 }
@@ -152,7 +108,7 @@ dev.off()
 # =======================================
 
 # Sorting species
-Y <- read.csv(file=file.path(out_dir, "Y.csv"))
+Y <- read.csv(file=file.path(out_dir, "Y.csv"), header=TRUE, row.names=1)
 Y_sort <- Y
 Y_sort[, 1] <- Y[, 4]
 Y_sort[, 2] <- Y[, 5]
@@ -205,7 +161,7 @@ for (j in 1:n_species) {
 ## Species effects beta
 pdf(file=file.path(out_dir, "pred_obs_beta_mod_2.pdf"), width=5, height=5)
 plot(t(beta_target), mean_beta,
-     main="species effect beta",
+     main="Species effect beta",
      xlab ="obs", ylab ="fitted")
 abline(a=0,b=1,col='red')
 dev.off()
@@ -215,7 +171,7 @@ pdf(file=file.path(out_dir, "pred_obs_lambda_mod_2.pdf"), width=10, height=10)
 par(mfrow=c(2, 2))
 for (l in 1:n_q) {
   plot(t(lambda_target)[, l], mean_lambda[, l],
-       main=paste0("factor loadings lambda", l),
+       main=paste0("Factor loadings lambda", l),
        xlab ="obs", ylab ="fitted")
   abline(a=0,b=1,col='red')
 }
